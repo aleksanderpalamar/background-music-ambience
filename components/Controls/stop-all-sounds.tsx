482cf61f3742e8dd555945e29f19420cfcd969e2
aleksanderpalamar <apalamar@live.com>
2024-01-05 18:57:53 -0300

    created fullscreen functionality

--- components/Controls/stop-all-sounds.tsx
+++ components/Controls/stop-all-sounds.tsx
@@ -2,11 +2,18 @@
 
 import { StopCircle } from "lucide-react";
 import { Button } from "../ui/button";
+import { useContext } from "react";
+import { PlayerContext } from "@/context/player-context";
 
 export const StopAllSounds = () => {
+  const { stopAllSounds } = useContext(PlayerContext);  
+
   return (
-    <div className="tooltip" data-tip="Stop all Sounds">
-      <Button className="bg-transparent border-none hover:bg-transparent">
+    <div className="tooltip">
+      <Button
+        className="bg-transparent border-none hover:bg-zinc-900/10 rounded-full p-2"
+        onClick={stopAllSounds}
+      >
         <StopCircle className="text-2xl" />
       </Button>
     </div>
