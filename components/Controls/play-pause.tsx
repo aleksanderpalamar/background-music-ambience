a9273f9e4becd83bcb8a8a842592a3c93d2d1233
aleksanderpalamar <apalamar@live.com>
2024-01-05 11:31:37 -0300

    created the player-context and the play and pause functionality

--- components/Controls/play-pause.tsx
+++ components/Controls/play-pause.tsx
@@ -2,26 +2,54 @@
 
 import { Pause, Play } from "lucide-react";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { PlayerContext } from "@/context/player-context";
 
 export const PlayPause = () => {
-  const [play, setPlay] = useState(true);  
-  
-  function playPause() {
-    if (play) {
-      setPlay(false);
+  const { player } = useContext(PlayerContext);
+  const [isPlaying, setIsPlaying] = useState(false);
+  let isVideoPlaying = false;
+
+  function playPauseMedia(mediaId: any) {
+    if (mediaId === "video") {
+      if (player) {
+        if (isVideoPlaying) {
+          if (player && typeof player.pauseVideo === "function") {
+            player.pauseVideo();
+          }
+          isVideoPlaying = false;
+        } else {
+          if (player && typeof player.playVideo === "function") {
+            player.playVideo();
+          }
+          isVideoPlaying = true;
+        }
+      }
+
+      setIsPlaying(!isVideoPlaying);
     } else {
-      setPlay(true);
+      const sound = document.getElementById(mediaId) as HTMLMediaElement;
+      if (sound) {
+        if (sound.paused) {
+          sound.play();
+        } else {
+          sound.pause();
+        }
+      }
     }
   }
 
   return (
     <div className="tooltip" data-tip="Play/Pause">
-      <Button 
-        className="bg-transparent border-none hover:bg-transparent"
-        onClick={playPause}
+      <Button
+        className="bg-transparent border-none hover:bg-zinc-900/10 rounded-full p-2"
+        onClick={() => playPauseMedia("video")}
       >
-        {play ? <Play className="text-2xl"/> : <Pause  className="text-2xl"/>} 
+        {isPlaying ? (
+          <Play role="button" className="text-2xl" />
+        ) : (
+          <Pause role="button" className="text-2xl" />
+        )}
       </Button>
     </div>
   );
